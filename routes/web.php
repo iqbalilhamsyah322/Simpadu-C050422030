e122efc196618a8facc700f24ca66b3cd8a85c93
Iqbal <iqbalilhamsyah32@gmail.com>
2023-11-05 09:17:35 +0800

    Stisla

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function() {
+    return view('pages.auth.auth-login', ['type_menu' => '']);
+});
+
+Route::middleware(['auth'])->group(function () {
+
+    Route::get('home', function () {
+        return view("pages.app.dashboard-simpadu", ['type_menu' => '']);
+    })->name('home');
+});
